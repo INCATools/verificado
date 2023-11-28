6d1ea524c11cc6e319fc34e662fbaac1afdef112
Anita Caron <anitac@ebi.ac.uk>
2023-11-28 18:02:29 +0000

    Refact code (#1)
    * update dependencies
    
    * updates for pylint
    
    * rearrange imports and remove field name
    
    ---------
    
    Co-authored-by: Anita Caron <anitacaron@users.noreply.github.com>

--- src/relation_validator/__main__.py
+++ src/relation_validator/__main__.py
@@ -1,34 +1,59 @@
 import argparse
 import pathlib
 
-from .validator import validate
-from .validator import ontologies_version
+from .validator import ontologies_version, validate
+
 
 def main():
-  parser = argparse.ArgumentParser()
-  subparsers = parser.add_subparsers(help='Available dosdp actions', dest='action')
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(
+        help='Available dosdp actions',
+        dest='action'
+    )
 
-  parser_validate = subparsers.add_parser("validate",
-                                          description="The validate parser",
-                                          help="Validates the pairs in filename using the relationships in the input file")
-  parser_validate.add_argument('-i', '--input', action='store', 
-                               type=pathlib.Path, required=True, 
-                               help="yaml file with config")
-  parser_validate.add_argument('-o', '--output', action='store', 
-                               type=pathlib.Path, required=True, 
-                               help="csv output filename")
+    parser_validate = subparsers.add_parser(
+        name="validate",
+        description="The validate parser",
+        help="Validates the pairs using the relationships in the input file"
+    )
+    parser_validate.add_argument(
+        "-i",
+        "--input",
+        action="store",
+        type=pathlib.Path,
+        required=True,
+        help="yaml file with config"
+    )
+    parser_validate.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        type=pathlib.Path,
+        required=True,
+        help="csv output filename"
+    )
 
-  parser_ont_versions = subparsers.add_parser("ontologies_version", add_help=True,
-                                              help="Ontologies' version available in Ubergraph")
-  parser_ont_versions.add_argument('-o', '--output', action='store', 
-                                   type=pathlib.Path, required=True, help="json output filename")
-  
-  args = parser.parse_args()
+    parser_ont_versions = subparsers.add_parser(
+        name="ontologies_version",
+        add_help=True,
+        help="Ontologies' version available in Ubergraph"
+    )
+    parser_ont_versions.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        type=pathlib.Path,
+        required=True,
+        help="json output filename"
+    )
 
-  if args.action == "validate":
-    validate(str(args.input), str(args.output))
-  elif args.action == "ontologies_version":
-    ontologies_version(args.output)
+    args = parser.parse_args()
+
+    if args.action == "validate":
+        validate(str(args.input), str(args.output))
+    elif args.action == "ontologies_version":
+        ontologies_version(args.output)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- src/relation_validator/utils/utils.py
+++ src/relation_validator/utils/utils.py
@@ -1,96 +1,154 @@
-import os
+"""
+Script with util function used in validator
+"""
 import logging
-from ruamel.yaml import YAML, YAMLError
+import os
 
 from oaklib.implementations.ubergraph import UbergraphImplementation
+from ruamel.yaml import YAML, YAMLError
 
 QUERY = """
-   VALUES (?subject ?object) {{
-      {pairs}
+    VALUES (?subject ?object) {{
+        {pairs}
     }}
     ?subject {property} ?object .
 """
 
+
 def query_ubergraph(query):
-  oi = UbergraphImplementation()
-  prefixes = get_prefixes(query, oi.prefix_map().keys())
-  
-  res = oi.query(query=query, prefixes=prefixes)
-  
-  return [r for r in res]
+    """
+    Query Ubergraph and return results
+    """
+    oi = UbergraphImplementation()
+    prefixes = get_prefixes(query, oi.prefix_map().keys())
+
+    res = oi.query(query=query, prefixes=prefixes)
+
+    return list(res)
+
 
 def get_pairs(data):
-  pairs = set()
-  for _, row in data.iterrows():
-    pairs.add(f"({row['s']} {row['o']})")
-  return pairs
+    """
+    Get pairs (s, o) from DataFrame
+    """
+    pairs = set()
+    for _, row in data.iterrows():
+        pairs.add(f"({row['s']} {row['o']})")
+    return pairs
+
 
 def chunks(lst, n):
-  for i in range(0, len(lst), n):
-      yield lst[i:i + n]
+    """
+    Chunk funtion
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
-def transform_to_str(list):
-  terms_pairs = set()
 
-  for s, o in list:
-    terms_pairs.add(f"({s} {o})")
-  return terms_pairs
+def transform_to_str(entry: list):
+    """
+    Transform the pairs in the list into string in the format "(s, o)"
+    """
+    terms_pairs = set()
 
-def split_terms(list):
-  terms_s = []
-  terms_o = []
+    for s, o in entry:
+        terms_pairs.add(f"({s} {o})")
 
-  for pairs in list:
-    s, o = pairs.split(" ")
-    terms_s.append(s[1:])
-    terms_o.append(o[:-1])
+    return terms_pairs
 
-  return terms_s, terms_o
 
-def extract_results(list):
-  return set((r["subject"], r["object"]) for r in list)
+def split_terms(entry: list) -> (list, list):
+    """
+    Return s and o terms
+    """
+    terms_s = []
+    terms_o = []
+
+    for pairs in entry:
+        s, o = pairs.split(" ")
+        terms_s.append(s[1:])
+        terms_o.append(o[:-1])
+
+    return terms_s, terms_o
+
+
+def extract_results(entry: list):
+    """
+    Extract values subject and object from result
+    """
+    return set((r["subject"], r["object"]) for r in entry)
 
 
 def verify_relationship(terms_pairs, relationship):
-  valid_relationship = set()
-  if len(terms_pairs) > 90:
-    for chunk in chunks(list(terms_pairs), 90):
-      valid_relationship = valid_relationship.union(extract_results(query_ubergraph(QUERY.format(pairs=" ".join(chunk), property=relationship))))
-  else:
-    valid_relationship = extract_results(query_ubergraph(QUERY.format(pairs=" ".join(list(terms_pairs)), property=relationship)))
-  
-  non_valid_relationship = terms_pairs - transform_to_str(valid_relationship)
+    """
+    Query Ubergraph with term pairs and relationship.
+    Return valid and not valid pairs.
+    """
+    valid_relationship = set()
+    if len(terms_pairs) > 90:
+        for chunk in chunks(list(terms_pairs), 90):
+            valid_relationship = valid_relationship.union(
+                extract_results(
+                    query_ubergraph(
+                        QUERY.format(
+                            pairs=" ".join(chunk), property=relationship
+                        )
+                    )
+                )
+            )
+    else:
+        valid_relationship = extract_results(
+            query_ubergraph(
+                QUERY.format(
+                    pairs=" ".join(list(terms_pairs)),
+                    property=relationship
+                )
+            )
+        )
 
-  return valid_relationship, non_valid_relationship
+    non_valid_relationship = terms_pairs - transform_to_str(valid_relationship)
+
+    return valid_relationship, non_valid_relationship
 
 
-def get_config(input) -> dict:
-  if input.endswith('.yaml') or input.endswith('.yml'):
-    config = os.path.abspath(input)
-    ryaml = YAML(typ='safe')
-    with open(config, "r") as f:
-      try:
-        config = ryaml.load(f)
-      except YAMLError as exc:
-        logging.info(f'Failed to load config: {config}')
-    return config
-  else:
+def get_config(entry) -> dict:
+    """
+    Parse config file
+    """
+    if entry.endswith('.yaml') or entry.endswith('.yml'):
+        config = os.path.abspath(entry)
+        ryaml = YAML(typ='safe')
+        with open(config, "r", encoding="utf-8") as f:
+            try:
+                config = ryaml.load(f)
+            except YAMLError:
+                logging.info("Failed to load config: %s", config)
+        return config
+
     logging.error("Given path has unsupported file extension.")
     return False
 
-def get_prefixes(text, prefix_map):
-  prefixes = []
-  for prefix in prefix_map:
-    if prefix in text:
-      prefixes.append(prefix)
 
-  return prefixes
+def get_prefixes(text, prefix_map):
+    """
+    Filter prefix only on the pair terms
+    """
+    prefixes = []
+    for prefix in prefix_map:
+        if prefix in text:
+            prefixes.append(prefix)
+
+    return prefixes
+
 
 def get_ontologies_version():
-  QUERY_VERSION = """
-    ?ontology a owl:Ontology .
-    OPTIONAL { ?ontology owl:versionIRI ?version . }
-  """
-  
-  response = query_ubergraph(QUERY_VERSION)
-  return response
+    """
+    Query Ubergraph all ontology and its version
+    """
+    QUERY_VERSION = """
+        ?ontology a owl:Ontology .
+        OPTIONAL { ?ontology owl:versionIRI ?version . }
+    """
+
+    response = query_ubergraph(QUERY_VERSION)
+    return response
