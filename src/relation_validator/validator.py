727ca01bfa4efe92d578b6999087ab59a3e33518
Anita Caron <anitacaron@users.noreply.github.com>
2024-01-26 15:29:45 -0300

    create run_validation function

--- src/relation_validator/validator.py
+++ src/relation_validator/validator.py
@@ -6,6 +6,24 @@ from .utils.utils import (get_config, get_ontologies_version, get_pairs,
                           split_terms, verify_relationship)
 
 
+def run_validation(data: pd.DataFrame, relationships: dict) -> pd.DataFrame:
+    """
+    Validation process for each relationship
+    """
+    terms_pairs = get_pairs(data)
+
+    for _, rel in relationships.items():
+        _, terms_pairs = verify_relationship(terms_pairs, rel)
+
+    terms_s, terms_o = split_terms(terms_pairs)
+
+    rows_nv = data[
+        data[["s", "o"]].apply(tuple, 1).isin(zip(terms_s, terms_o))
+    ]
+
+    return rows_nv
+
+
 def validate(args):
     """
     Validate process
@@ -15,18 +33,10 @@ def validate(args):
         return exit
 
     data = pd.read_csv(config["filename"])
-    terms_pairs = get_pairs(data)
 
-    for _, rel in config["relationships"].items():
-        _, terms_pairs = verify_relationship(terms_pairs, rel)
+    report = run_validation(data, config["relationships"])
 
-    terms_s, terms_o = split_terms(terms_pairs)
-
-    rows_nv = data[
-        data[["s", "o"]].apply(tuple, 1).isin(zip(terms_s, terms_o))
-    ]
-
-    rows_nv.to_csv(args.output, index=False)
+    report.to_csv(args.output, index=False)
 
 
 def ontologies_version(args):
